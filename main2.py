09aadb6abea41faa147b345aa1d2913c643eaddc
matsue-code <mbolamathew@icloud.com>
2025-09-21 10:40:35 +0300

    second commit

---
+++ main2.py
@@ -0,0 +1,13 @@
+class Employee:
+    def __init__(self):
+        print('employee created')
+    def __del__(self):
+        print("destructor called")
+def create_obj():
+    print('making object...')
+    obj = Employee()
+    print('function end...')
+    return obj
+print('calling create_obj() function...')
+obj = create_obj()
+print('program end...')
